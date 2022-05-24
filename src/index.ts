55b025339c38b4b954c63f51376f36bcd6cbaf78
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2022-05-24 23:28:48 +0200

    feat: winston loggers

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import { AppDataSource } from './data-source';
 import { Performance } from './models/Performance.entity';
 import { login, signup } from './controllers/user';
 import { handleError } from './errors/handleError';
+import { requestLogger } from './middlwares/logger';
+import { errorLogger } from 'express-winston';
 
 dotenv.config();
 const PORT = process.env.PORT || 3005;
@@ -17,6 +19,17 @@ const wss = new WebSocket.Server({ server });
 AppDataSource.initialize()
   .then(() => console.log('connection to DB established'))
   .catch((e) => console.log(e));
+
+const testQuery = async () => {
+  return await AppDataSource.getRepository(Performance)
+    .createQueryBuilder('performance')
+    .leftJoinAndSelect('performance.category', 'category')
+    .leftJoinAndSelect('performance.contestant', 'contestant')
+    .leftJoinAndSelect('performance.scores', 'scores')
+    .leftJoinAndSelect('scores.judge', 'judge')
+    .leftJoinAndSelect('scores.criteria', 'criteria')
+    .getMany();
+};
 
 wss.on('connection', (ws: WebSocket) => {
   ws.on('message', async (message: string) => {
@@ -31,8 +44,15 @@ wss.on('connection', (ws: WebSocket) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Request logger
+app.use(requestLogger);
+
+// Auth
 app.post('/login', login);
 app.post('/signup', signup);
+
+// Errors logger
+app.use(errorLogger);
 
 // Errors handler
 app.use(
@@ -47,14 +67,3 @@ app.use(
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT} :)`);
 });
-
-const testQuery = async () => {
-  return await AppDataSource.getRepository(Performance)
-    .createQueryBuilder('performance')
-    .leftJoinAndSelect('performance.category', 'category')
-    .leftJoinAndSelect('performance.contestant', 'contestant')
-    .leftJoinAndSelect('performance.scores', 'scores')
-    .leftJoinAndSelect('scores.judge', 'judge')
-    .leftJoinAndSelect('scores.criteria', 'criteria')
-    .getMany();
-};
